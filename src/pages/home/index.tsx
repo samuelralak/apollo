bec8be9538db84e51117d9321a6318d6c64579d0
Samuel Ralak <thesamuelralak@gmail.com>
2023-12-31 04:16:19 +0300

    Problem: No empty state message
    Solution:
    Add message when home state is empty

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -5,6 +5,7 @@ import {transformer as questionTransformer} from "../../resources/question";
 import Loader from "../../components/Loader";
 import QuestionsList from "../../components/questions/QuestionsList";
 import constants from "../../constants";
+import NoQuestion from '../../assets/no-questions.svg'
 
 const Page = () => {
     const {ndkInstance} = useContext(NDKContext) as NDKContext
@@ -24,6 +25,18 @@ const Page = () => {
 
     return (
         <div className="mx-auto max-w-3xl">
+            {questionEvents && questionEvents.length === 0 && (
+                <div>
+                    <img src={NoQuestion} alt={'placeholder'} className="h-72 w-72 mx-auto"/>
+                    <h1 className="text-2xl text-slate-600 font-extrabold w-full text-center">
+                        Got a question? Ask Away!
+                    </h1>
+                    <p className="text-lg w-full text-center font-semibold max-w-xl mx-auto text-slate-400">
+                        The Apollo community is here to help. Your curiosity fuels our collaborative spirit
+                    </p>
+                </div>
+            )}
+
             {questionEvents ? (<QuestionsList questions={questions}/>) : <Loader/>}
         </div>
     )
