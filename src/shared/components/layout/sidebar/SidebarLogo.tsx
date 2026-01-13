0778de066176e1dac1ae59bfa60335e852f7861e
Samuel Ralak <thesamuelralak@gmail.com>
2026-01-13 18:08:00 +0300

    Add logo and favicon assets

--- src/shared/components/layout/sidebar/SidebarLogo.tsx
+++ src/shared/components/layout/sidebar/SidebarLogo.tsx
@@ -1,12 +1,22 @@
 import { Link } from "react-router";
+import LogoIcon from "../../../../assets/logo-icon.svg";
+import LogoIconDark from "../../../../assets/logo-icon-dark.svg";
+import LogoFull from "../../../../assets/logo-full.svg";
+import LogoFullDark from "../../../../assets/logo-full-dark.svg";
 
 const SidebarLogo = () => {
     return (
         <div className="h-14 flex items-center px-3 lg:px-4">
             <Link to="/" className="flex items-center">
-                <span className="text-slate-900 dark:text-white font-extrabold font-['Poppins'] text-base">
-                    <span className="lg:hidden">[A]</span>
-                    <span className="hidden lg:inline">[APOLLO]</span>
+                {/* Collapsed: Icon only */}
+                <span className="lg:hidden">
+                    <img src={LogoIcon} alt="Apollo" className="h-10 w-10 dark:hidden" />
+                    <img src={LogoIconDark} alt="Apollo" className="h-10 w-10 hidden dark:block" />
+                </span>
+                {/* Expanded: Full logo with wordmark */}
+                <span className="hidden lg:block">
+                    <img src={LogoFull} alt="Apollo" className="h-9 dark:hidden" />
+                    <img src={LogoFullDark} alt="Apollo" className="h-9 hidden dark:block" />
                 </span>
             </Link>
         </div>
